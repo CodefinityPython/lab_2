64ee0ed5cd98e5aff52e0a61a4a021e36e6cccaa
HS-user <hs-user@HS-users-MacBook-Pro-2.local>
2023-10-11 21:39:26 +0300

    sol

--- main.py
+++ main.py
@@ -2,19 +2,23 @@
 
 
 # Створення порожнього списку для балів студентів
-
+scores = []
 
 # Запит користувача на введення кількості студентів у групі
-
+students = int( input('Кількість студентів:') )
 
 # Зчитування балів студентів та додавання їх до списку
-
-
+for i in range(students) :
+    ball = float(input('Бал студента:'))   
+    scores.append(ball) 
 # Обчислення суми балів
-
+ball_sum = sum(scores)
 
 # Обчислення середнього балу
-
+#ball_ser = str( ball_sum / students)
+ball_ser = ball_sum / students
 
 # Виведення результату на екран
-
+#print(f'Середній бал студентів:{ball_ser} ')
+#print('Середній бал студентів:' + ball_ser ) 
+print('Середній бал студентів:', ball_ser )
