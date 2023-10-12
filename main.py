4e2c770e3240ae7ae0553092f10a4351946748e7
hs-user <hs-user@hs-users-MacBook-Pro.local>
2023-10-12 16:04:09 +0300

    Solution

--- main.py
+++ main.py
@@ -2,19 +2,21 @@
 
 
 # Створення порожнього списку для балів студентів
-
+scores = []
 
 # Запит користувача на введення кількості студентів у групі
-
+num_students = int(input("Введіть кількість студентів у групі: "))
 
 # Зчитування балів студентів та додавання їх до списку
-
+for i in range(num_students):
+    score = float(input(f"Введіть бали студента {i+1}: "))
+    scores.append(score)
 
 # Обчислення суми балів
-
+total_score = sum(scores)
 
 # Обчислення середнього балу
-
+average_score = total_score / num_students
 
 # Виведення результату на екран
-
+print(f"Середній бал: {average_score}")
